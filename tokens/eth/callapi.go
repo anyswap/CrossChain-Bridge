999d8ca34e2731988d733d6f89683f7b13a11f5c
cross-chain <xiao.zongyuan@fusion.org>
2020-04-27 14:44:10 +0800

    update eth call api

--- tokens/eth/callapi.go
+++ tokens/eth/callapi.go
@@ -1,1 +1,85 @@
 package eth
+
+import (
+	"math/big"
+
+	"github.com/fsn-dev/crossChain-Bridge/common"
+	"github.com/fsn-dev/crossChain-Bridge/common/hexutil"
+	"github.com/fsn-dev/crossChain-Bridge/rlp"
+	"github.com/fsn-dev/crossChain-Bridge/rpc/client"
+)
+
+func (b *EthBridge) GetLatestBlockNumber() (uint64, error) {
+	_, gateway := b.GetTokenAndGateway()
+	url := gateway.ApiAddress
+	var result string
+	err := client.RpcPost(&result, url, "eth_blockNumber")
+	if err != nil {
+		return 0, err
+	}
+	return common.GetUint64FromStr(result)
+}
+
+func (b *EthBridge) GetBlockByHash(blockHash string) (*RPCBlock, error) {
+	_, gateway := b.GetTokenAndGateway()
+	url := gateway.ApiAddress
+	var result RPCBlock
+	err := client.RpcPost(&result, url, "eth_getBlockByHash", blockHash, false)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (b *EthBridge) GetTransaction(txHash string) (*RPCTransaction, error) {
+	_, gateway := b.GetTokenAndGateway()
+	url := gateway.ApiAddress
+	var result RPCTransaction
+	err := client.RpcPost(&result, url, "eth_getTransactionByHash", txHash)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (b *EthBridge) GetTransactionReceipt(txHash string) (*RPCTxReceipt, error) {
+	_, gateway := b.GetTokenAndGateway()
+	url := gateway.ApiAddress
+	var result RPCTxReceipt
+	err := client.RpcPost(&result, url, "eth_getTransactionReceipt", txHash)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (b *EthBridge) GetPoolNonce(address string) (uint64, error) {
+	_, gateway := b.GetTokenAndGateway()
+	url := gateway.ApiAddress
+	account := common.HexToAddress(address)
+	var result hexutil.Uint64
+	err := client.RpcPost(&result, url, "eth_getTransactionCount", account, "pending")
+	return uint64(result), err
+}
+
+func (b *EthBridge) SuggestPrice() (*big.Int, error) {
+	_, gateway := b.GetTokenAndGateway()
+	url := gateway.ApiAddress
+	var result hexutil.Big
+	err := client.RpcPost(&result, url, "eth_gasPrice")
+	if err != nil {
+		return nil, err
+	}
+	return result.ToInt(), nil
+}
+
+func (b *EthBridge) SendSignedTransaction(tx *Transaction) error {
+	data, err := rlp.EncodeToBytes(tx)
+	if err != nil {
+		return err
+	}
+	_, gateway := b.GetTokenAndGateway()
+	url := gateway.ApiAddress
+	var result interface{}
+	return client.RpcPost(&result, url, "eth_sendRawTransaction", common.ToHex(data))
+}
---
+++ tokens/eth/types.go
@@ -0,0 +1,74 @@
+package eth
+
+import (
+	"github.com/fsn-dev/crossChain-Bridge/common"
+	"github.com/fsn-dev/crossChain-Bridge/common/hexutil"
+)
+
+type RPCBlock struct {
+	Hash            *common.Hash    `json:"hash"`
+	ParentHash      *common.Hash    `json:"parentHash"`
+	UncleHash       *common.Hash    `json:"sha3Uncles"`
+	Coinbase        *common.Address `json:"miner"`
+	Root            *common.Hash    `json:"stateRoot"`
+	TxHash          *common.Hash    `json:"transactionsRoot"`
+	ReceiptHash     *common.Hash    `json:"receiptsRoot"`
+	Bloom           *hexutil.Bytes  `json:"logsBloom"`
+	Difficulty      *hexutil.Big    `json:"difficulty"`
+	Number          *hexutil.Big    `json:"number"`
+	GasLimit        *hexutil.Uint64 `json:"gasLimit"`
+	GasUsed         *hexutil.Uint64 `json:"gasUsed"`
+	Time            *hexutil.Big    `json:"timestamp"`
+	Extra           *hexutil.Bytes  `json:"extraData"`
+	MixDigest       *common.Hash    `json:"mixHash"`
+	Nonce           *hexutil.Bytes  `json:"nonce"`
+	Size            *string         `json:"size"`
+	TotalDifficulty *hexutil.Big    `json:"totalDifficulty"`
+	Transactions    []*common.Hash  `json:"transactions"`
+	Uncles          []*common.Hash  `json:"uncles"`
+}
+
+type RPCTransaction struct {
+	Hash             *common.Hash    `json:"hash"`
+	TransactionIndex *hexutil.Uint   `json:"transactionIndex"`
+	BlockNumber      *hexutil.Big    `json:"blockNumber,omitempty"`
+	BlockHash        *common.Hash    `json:"blockHash,omitempty"`
+	From             *common.Address `json:"from,omitempty"`
+	AccountNonce     *hexutil.Uint64 `json:"nonce"`
+	Price            *hexutil.Big    `json:"gasPrice"`
+	GasLimit         *hexutil.Uint64 `json:"gas"`
+	Recipient        *common.Address `json:"to"`
+	Amount           *hexutil.Big    `json:"value"`
+	Payload          *hexutil.Bytes  `json:"input"`
+	V                *hexutil.Big    `json:"v"`
+	R                *hexutil.Big    `json:"r"`
+	S                *hexutil.Big    `json:"s"`
+}
+
+type RPCLog struct {
+	Address     *common.Address `json:"address"`
+	Topics      []common.Hash   `json:"topics"`
+	Data        *hexutil.Bytes  `json:"data"`
+	BlockNumber *hexutil.Uint64 `json:"blockNumber"`
+	TxHash      *common.Hash    `json:"transactionHash"`
+	TxIndex     *hexutil.Uint   `json:"transactionIndex"`
+	BlockHash   *common.Hash    `json:"blockHash"`
+	Index       *hexutil.Uint   `json:"logIndex"`
+	Removed     *bool           `json:"removed"`
+}
+
+type RPCTxReceipt struct {
+	TxHash            *common.Hash    `json:"transactionHash"`
+	TxIndex           *hexutil.Uint   `json:"transactionIndex"`
+	BlockNumber       *hexutil.Big    `json:"blockNumber"`
+	BlockHash         *common.Hash    `json:"blockHash"`
+	PostState         *hexutil.Bytes  `json:"root"`
+	Status            *hexutil.Uint64 `json:"status"`
+	From              *common.Address `json:"from"`
+	Recipient         *common.Address `json:"to"`
+	GasUsed           *hexutil.Uint64 `json:"gasUsed"`
+	CumulativeGasUsed *hexutil.Uint64 `json:"cumulativeGasUsed"`
+	ContractAddress   *common.Address `json:"contractAddress,omitempty"`
+	Bloom             *hexutil.Bytes  `json:"logsBloom"`
+	Logs              []*RPCLog       `json:"logs"`
+}
