5bda6555810b73a044e2a424d3d6b950d6406202
cross-chain <xiao.zongyuan@fusion.org>
2020-04-29 13:20:09 +0800

    update fsn sendtx

--- tokens/fsn/sendtx.go
+++ tokens/fsn/sendtx.go
@@ -1,9 +1,17 @@
 package fsn
 
 import (
-//. "github.com/fsn-dev/crossChain-Bridge/tokens"
+	"errors"
 )
 
 func (b *FsnBridge) SendTransaction(signedTx interface{}) (txHash string, err error) {
-	return "", nil
+	tx, ok := signedTx.(*Transaction)
+	if !ok {
+		return "", errors.New("wrong signed transaction type")
+	}
+	err = b.SendSignedTransaction(tx)
+	if err != nil {
+		return "", err
+	}
+	return tx.Hash().String(), nil
 }
