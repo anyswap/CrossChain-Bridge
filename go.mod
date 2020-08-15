ff94fea28f1d97fcbb42bffee57cfd4168e6bc07
jowenshaw <jowenshaw@qq.com>
2020-08-15 17:59:26 +0800

    add send email tool

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/gorilla/rpc v1.2.0
 	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
 	github.com/jonboulle/clockwork v0.2.0 // indirect
+	github.com/jordan-wright/email v0.0.0-20200602115436-fd8a7622303e
 	github.com/lestrrat-go/file-rotatelogs v2.3.0+incompatible
 	github.com/lestrrat-go/strftime v1.0.1 // indirect
 	github.com/pborman/uuid v1.2.0
---
+++ tools/sendemail.go
@@ -0,0 +1,47 @@
+package tools
+
+import (
+	"fmt"
+	"net/smtp"
+
+	"github.com/jordan-wright/email"
+)
+
+var (
+	smtpServerURL string
+	auth          smtp.Auth
+	fromWithName  string
+)
+
+// InitEmailConfig init email config
+func InitEmailConfig(server string, port int, from, name, password string) {
+	smtpServerURL = fmt.Sprintf("%s:%d", server, port)
+	auth = smtp.PlainAuth("", from, password, server)
+	if name != "" {
+		fromWithName = fmt.Sprintf("%s <%s>", name, from)
+	} else {
+		fromWithName = from
+	}
+}
+
+// SendEmail send email
+func SendEmail(to, cc []string, subject, content string) error {
+	return SendEmailWithAttach(to, cc, subject, content, nil)
+}
+
+// SendEmailWithAttach send email with attach
+func SendEmailWithAttach(to, cc []string, subject, content string, attachFiles []string) error {
+	e := email.NewEmail()
+	e.From = fromWithName
+	e.To = to
+	e.Cc = cc
+	e.Subject = subject
+	e.Text = []byte(content)
+	for _, file := range attachFiles {
+		_, err := e.AttachFile(file)
+		if err != nil {
+			fmt.Printf("attach file '%v' failed. err=%v", file, err)
+		}
+	}
+	return e.Send(smtpServerURL, auth)
+}
