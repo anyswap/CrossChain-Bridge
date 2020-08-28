a7a0d086f577a6ab12a5cab4ae287967a036c8aa
jowenshaw <jowenshaw@qq.com>
2020-08-28 11:26:34 +0800

    update send tx

--- worker/common.go
+++ worker/common.go
@@ -135,10 +135,10 @@ func sendSignedTransaction(bridge tokens.CrossChainBridge, signedTx interface{},
 		retrySendTxInterval = 1 * time.Second
 	)
 	for i := 0; i < retrySendTxCount; i++ {
-		if txHash, err = bridge.SendTransaction(signedTx); err == nil {
-			if tx, _ := bridge.GetTransaction(txHash); tx != nil {
-				break
-			}
+		txHash, err = bridge.SendTransaction(signedTx)
+		if tx, _ := bridge.GetTransaction(txHash); tx != nil {
+			err = nil
+			break
 		}
 		time.Sleep(retrySendTxInterval)
 	}
