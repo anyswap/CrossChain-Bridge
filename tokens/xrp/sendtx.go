c3ca2d6e56e70cb2246093333cebd94f76a3507b
gaozhengxin <zhengxingao@live.cn>
2020-12-28 17:15:21 +0800

    add xrp send transaction

--- tokens/xrp/sendtx.go
+++ tokens/xrp/sendtx.go
@@ -1,6 +1,30 @@
 package xrp
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/rubblelabs/ripple/data"
+)
+
 // SendTransaction send signed tx
 func (b *Bridge) SendTransaction(signedTx interface{}) (txHash string, err error) {
-	return "", nil
+	tx, ok := signedTx.(data.Transaction)
+	if !ok {
+		return "", fmt.Errorf("Send transaction type assertion error")
+	}
+	for i := 0; i < rpcRetryTimes; i++ {
+		for _, r := range b.Remotes {
+			resp, err1 := r.Submit(tx)
+			if err1 != nil || resp == nil {
+				err = err1
+				continue
+			}
+			if resp.EngineResult == 0 {
+				return tx.GetBase().Hash.String(), nil
+			}
+		}
+		time.Sleep(rpcRetryInterval)
+	}
+	return
 }
