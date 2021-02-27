1fc419b0b8bacd65a13aab605244c5da1e61a99d
jowenshaw <jowenshaw@gmail.com>
2021-02-27 14:27:02 +0800

    add a small bias value for big value threshold

--- tokens/types.go
+++ tokens/types.go
@@ -346,7 +346,7 @@ func (c *TokenConfig) CalcAndStoreValue() {
 	c.minSwap = ToBits(*c.MinimumSwap, *c.Decimals)
 	c.maxSwapFee = ToBits(*c.MaximumSwapFee, *c.Decimals)
 	c.minSwapFee = ToBits(*c.MinimumSwapFee, *c.Decimals)
-	c.bigValThreshhold = ToBits(*c.BigValueThreshold, *c.Decimals)
+	c.bigValThreshhold = ToBits(*c.BigValueThreshold+0.0001, *c.Decimals)
 }
 
 // GetDcrmAddressPrivateKey get private key
