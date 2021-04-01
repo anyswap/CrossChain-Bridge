a53be7d8c510a1b553f138db19a9a9ccf3cc2e19
jowenshaw <jowenshaw@gmail.com>
2021-04-01 17:33:25 +0800

    update IsContractAddress for RSK chain

--- tokens/eth/address.go
+++ tokens/eth/address.go
@@ -33,7 +33,7 @@ func (b *Bridge) IsContractAddress(address string) (bool, error) {
 	for i := 0; i < retryRPCCount; i++ {
 		code, err = b.GetCode(address)
 		if err == nil {
-			return len(code) != 0, nil
+			return len(code) > 1, nil // unexpect RSK getCode return 0x00
 		}
 		time.Sleep(retryRPCInterval)
 	}
