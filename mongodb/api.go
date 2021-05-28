b4033555b3e93ac9cb863f7405db15c06f4a549c
jowenshaw <jowenshaw@gmail.com>
2021-05-28 10:10:58 +0800

    enhance update swaptx lock

--- mongodb/api.go
+++ mongodb/api.go
@@ -378,7 +378,7 @@ func updateSwapResult(collection *mgo.Collection, txid, pairID, bind string, ite
 	} else if items.Status == MatchTxNotStable {
 		updates["memo"] = ""
 	}
-	if items.SwapNonce != 0 {
+	if items.SwapNonce != 0 || items.Status == MatchTxNotStable {
 		updateResultLock.Lock()
 		defer updateResultLock.Unlock()
 		swapRes, err := findSwapResult(collection, txid, pairID, bind)
@@ -389,7 +389,13 @@ func updateSwapResult(collection *mgo.Collection, txid, pairID, bind string, ite
 			log.Error("forbid update swap nonce again", "old", swapRes.SwapNonce, "new", items.SwapNonce)
 			return ErrForbidUpdateNonce
 		}
-		updates["swapnonce"] = items.SwapNonce
+		if swapRes.SwapTx != "" {
+			log.Error("forbid update swap tx again", "old", swapRes.SwapTx, "new", items.SwapTx)
+			return ErrForbidUpdateSwapTx
+		}
+		if items.SwapNonce != 0 {
+			updates["swapnonce"] = items.SwapNonce
+		}
 	}
 	err := collection.UpdateId(GetSwapKey(txid, pairID, bind), bson.M{"$set": updates})
 	if err == nil {
--- mongodb/errors.go
+++ mongodb/errors.go
@@ -29,9 +29,10 @@ func mgoError(err error) error {
 
 // mongodb special errors
 var (
-	ErrItemNotFound      = newError(-32002, "mgoError: Item not found")
-	ErrItemIsDup         = newError(-32003, "mgoError: Item is duplicate")
-	ErrSwapNotFound      = newError(-32011, "mgoError: Swap is not found")
-	ErrWrongKey          = newError(-32012, "mgoError: Wrong key")
-	ErrForbidUpdateNonce = newError(-32013, "mgoError: Forbid update swap nonce")
+	ErrItemNotFound       = newError(-32002, "mgoError: Item not found")
+	ErrItemIsDup          = newError(-32003, "mgoError: Item is duplicate")
+	ErrSwapNotFound       = newError(-32011, "mgoError: Swap is not found")
+	ErrWrongKey           = newError(-32012, "mgoError: Wrong key")
+	ErrForbidUpdateNonce  = newError(-32013, "mgoError: Forbid update swap nonce")
+	ErrForbidUpdateSwapTx = newError(-32014, "mgoError: Forbid update swap tx")
 )
