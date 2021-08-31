3da84447d7a3f4a65349fc1b7c629aa369d866d1
jowenshaw <jowenshaw@gmail.com>
2021-08-31 10:20:47 +0800

    Revert "set no unlock memo in building swapout eth swaptx" (noncompat)
    This reverts commit d4aa2e4989384b832eb03164e14a5f8a1a3899a6.

--- tokens/eth/buildtx.go
+++ tokens/eth/buildtx.go
@@ -57,6 +57,7 @@ func (b *Bridge) BuildRawTransaction(args *tokens.BuildTxArgs) (rawTx interface{
 				input = *args.Input
 			} else {
 				args.To = args.Bind
+				input = b.getUnlockCoinMemo(args)
 			}
 		}
 	} else {
@@ -72,6 +73,14 @@ func (b *Bridge) BuildRawTransaction(args *tokens.BuildTxArgs) (rawTx interface{
 	}
 
 	return b.buildTx(args, extra, input)
+}
+
+func (b *Bridge) getUnlockCoinMemo(args *tokens.BuildTxArgs) (input []byte) {
+	isContract, err := b.IsContractAddress(args.Bind)
+	if err == nil && !isContract {
+		input = []byte(tokens.UnlockMemoPrefix + args.SwapID)
+	}
+	return input
 }
 
 func (b *Bridge) buildTx(args *tokens.BuildTxArgs, extra *tokens.EthExtraArgs, input []byte) (rawTx interface{}, err error) {
