8f3f3544994cda52dd8421e41cf03d933613a4a6
jowenshaw <jowenshaw@gmail.com>
2021-08-31 17:06:50 +0800

    update using base fee percent in building swaptx

--- tokens/base.go
+++ tokens/base.go
@@ -4,6 +4,7 @@ import (
 	"math"
 	"math/big"
 
+	"github.com/anyswap/CrossChain-Bridge/log"
 	"github.com/anyswap/CrossChain-Bridge/types"
 )
 
@@ -189,13 +190,14 @@ func CalcSwappedValue(pairID string, value *big.Int, isSrc bool) *big.Int {
 		swapFee = token.maxSwapFee
 	}
 
-	if GetNonceSetter(isSrc) != nil { // eth-like
-		chainCfg := GetCrossChainBridge(isSrc).GetChainConfig()
+	var adjustBaseFee *big.Int
+	if GetNonceSetter(!isSrc) != nil { // eth-like
+		chainCfg := GetCrossChainBridge(!isSrc).GetChainConfig()
 		if chainCfg.BaseFeePercent != 0 && token.minSwapFee.Sign() > 0 {
-			adjustBaseFee := new(big.Int).Set(token.minSwapFee)
+			adjustBaseFee = new(big.Int).Set(token.minSwapFee)
 			adjustBaseFee.Mul(adjustBaseFee, big.NewInt(chainCfg.BaseFeePercent))
 			adjustBaseFee.Div(adjustBaseFee, big.NewInt(100))
-			swapFee.Add(swapFee, adjustBaseFee)
+			swapFee = new(big.Int).Add(swapFee, adjustBaseFee)
 			if swapFee.Sign() < 0 {
 				swapFee = big.NewInt(0)
 			}
@@ -203,6 +205,8 @@ func CalcSwappedValue(pairID string, value *big.Int, isSrc bool) *big.Int {
 	}
 
 	if value.Cmp(swapFee) <= 0 {
+		log.Warn("check swap value failed", "pairID", pairID, "value", value, "isSrc", isSrc,
+			"minSwapFee", token.minSwapFee, "adjustBaseFee", adjustBaseFee, "swapFee", swapFee)
 		return big.NewInt(0)
 	}
 
