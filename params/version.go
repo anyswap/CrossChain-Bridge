338e89a6c114233d09eb74386eb250827f3620ae
jowenshaw <jowenshaw@gmail.com>
2021-09-15 10:40:43 +0800

    modify version to 0.3.6-r1

--- params/version.go
+++ params/version.go
@@ -6,10 +6,10 @@ import (
 
 // version parts
 const (
-	VersionMajor = 0  // Major version component of the current release
-	VersionMinor = 3  // Minor version component of the current release
-	VersionPatch = 6  // Patch version component of the current release
-	VersionMeta  = "" // Version metadata to append to the version string
+	VersionMajor = 0    // Major version component of the current release
+	VersionMinor = 3    // Minor version component of the current release
+	VersionPatch = 6    // Patch version component of the current release
+	VersionMeta  = "r1" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
