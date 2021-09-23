e80fc2e2c63ac66098a8f619551bd50889d7b1e0
jowenshaw <jowenshaw@gmail.com>
2021-09-23 15:30:18 +0800

    update dcrm init

--- dcrm/init.go
+++ dcrm/init.go
@@ -177,7 +177,7 @@ func initSelfEnode() {
 			return
 		}
 		log.Error("can't get enode info", "rpcAddr", defaultDcrmNode.dcrmRPCAddress, "err", err)
-		time.Sleep(3 * time.Second)
+		time.Sleep(10 * time.Second)
 	}
 }
 
@@ -208,7 +208,7 @@ func verifySignGroupInfo(rpcAddr, groupID string, isSignGroup, includeSelf bool)
 		groupInfo, err := GetGroupByID(groupID, rpcAddr)
 		if err != nil {
 			log.Error("get group info failed", "groupID", groupID, "err", err)
-			time.Sleep(3 * time.Second)
+			time.Sleep(10 * time.Second)
 			continue
 		}
 		log.Info("get dcrm group info success", "groupInfo", groupInfo)
@@ -216,9 +216,7 @@ func verifySignGroupInfo(rpcAddr, groupID string, isSignGroup, includeSelf bool)
 			log.Fatal("dcrm group member count mismatch", "groupID", dcrmGroupID, "have", groupInfo.Count, "want", memberCount)
 		}
 		if uint32(len(groupInfo.Enodes)) != memberCount {
-			log.Error("get group info enodes count mismatch", "groupID", groupID, "have", len(groupInfo.Enodes), "want", memberCount)
-			time.Sleep(3 * time.Second)
-			continue
+			log.Fatal("get group info enodes count mismatch", "groupID", groupID, "have", len(groupInfo.Enodes), "want", memberCount)
 		}
 		exist := isEnodeExistIn(selfEnode, groupInfo.Enodes)
 		if exist != includeSelf {
