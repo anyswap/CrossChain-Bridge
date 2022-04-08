ec76d36be3db22c96163037da9146f6c019efb7c
jowenshaw <jowenshaw@qq.com>
2022-04-08 17:42:57 +0800

    terra: impl GetLatestBlockNumber api

--- tokens/terra/callapi.go
+++ tokens/terra/callapi.go
@@ -1,15 +1,58 @@
 package terra
 
 import (
+	"errors"
+
+	"github.com/anyswap/CrossChain-Bridge/rpc/client"
 	"github.com/anyswap/CrossChain-Bridge/tokens"
+)
+
+var (
+	errEmptyURLs = errors.New("empty URLs")
+
+	wrapRPCQueryError = tokens.WrapRPCQueryError
+
+	rpcTimeout = 60
 )
 
 // GetLatestBlockNumberOf impl
 func (b *Bridge) GetLatestBlockNumberOf(apiAddress string) (uint64, error) {
-	return 0, tokens.ErrTodo
+	var result GetBlockResponse
+	err := client.RPCGetWithTimeout(&result, apiAddress, rpcTimeout)
+	if err != nil {
+		return 0, err
+	}
+	return uint64(result.Block.Header.Height), nil
 }
 
 // GetLatestBlockNumber impl
 func (b *Bridge) GetLatestBlockNumber() (uint64, error) {
-	return 0, tokens.ErrTodo
+	gateway := b.GatewayConfig
+	maxHeight, err := getMaxLatestBlockNumber(gateway.APIAddress)
+	if maxHeight > 0 {
+		tokens.CmpAndSetLatestBlockHeight(maxHeight, b.IsSrcEndpoint())
+		return maxHeight, nil
+	}
+	return 0, err
+}
+
+func getMaxLatestBlockNumber(urls []string) (maxHeight uint64, err error) {
+	if len(urls) == 0 {
+		return 0, errEmptyURLs
+	}
+	var result GetBlockResponse
+	path := "/cosmos/base/tendermint/v1beta1/blocks/latest"
+	for _, url := range urls {
+		err := client.RPCGetWithTimeout(&result, url+path, rpcTimeout)
+		if err == nil {
+			height := uint64(result.Block.Header.Height)
+			if height > maxHeight {
+				maxHeight = height
+			}
+		}
+	}
+	if maxHeight > 0 {
+		return maxHeight, nil
+	}
+	return 0, wrapRPCQueryError(err, path)
 }
--- tokens/terra/types.go
+++ tokens/terra/types.go
@@ -1,5 +1,46 @@
 package terra
 
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/anyswap/CrossChain-Bridge/common"
+)
+
+// GetBlockResponse get block response
+type GetBlockResponse struct {
+	Block *Block `json:"block"`
+}
+
+// Block block
+type Block struct {
+	Header *Header `json:"header"`
+}
+
+// Header header
+type Header struct {
+	ChainID string    `json:"chain_id"`
+	Height  int64     `json:"height"`
+	Time    time.Time `json:"time"`
+}
+
+func (h *Header) UnmarshalJSON(data []byte) error {
+	var head struct {
+		ChainID string    `json:"chain_id"`
+		Height  string    `json:"height"`
+		Time    time.Time `json:"time"`
+	}
+	err := json.Unmarshal(data, &head)
+	if err != nil {
+		return err
+	}
+	h.ChainID = head.ChainID
+	h.Time = head.Time
+	biHeight, err := common.GetBigIntFromStr(head.Height)
+	h.Height = biHeight.Int64()
+	return err
+}
+
 // GetTxResponse gettx response
 type GetTxResponse struct {
 	Tx         Tx         `json:"tx"`
