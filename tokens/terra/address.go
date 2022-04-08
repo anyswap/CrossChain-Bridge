cf1a1aa5e5c028aebe1d94693aee455903235d43
jowenshaw <jowenshaw@qq.com>
2022-04-08 11:13:18 +0800

    update terra address related methods using cosmos-sdk 0.44.7

--- tokens/terra/address.go
+++ tokens/terra/address.go
@@ -1,6 +1,41 @@
 package terra
 
+import (
+	"encoding/hex"
+	"strings"
+
+	"github.com/btcsuite/btcd/btcec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 // IsValidAddress check address
-func (b *Bridge) IsValidAddress(addr string) bool {
-	return true
+func (b *Bridge) IsValidAddress(address string) bool {
+	_, err := sdk.AccAddressFromBech32(address)
+	return err == nil
+}
+
+// EqualAddress equal address
+func (b *Bridge) EqualAddress(address1, address2 string) bool {
+	acc1, err1 := sdk.AccAddressFromBech32(address1)
+	acc2, err2 := sdk.AccAddressFromBech32(address2)
+	return err1 == nil && err2 == nil && acc1.Equals(acc2)
+}
+
+// PublicKeyToAddress returns cosmos public key address
+func (b *Bridge) PublicKeyToAddress(pubKeyHex string) (string, error) {
+	pubKeyHex = strings.TrimPrefix(pubKeyHex, "0x")
+	bs, err := hex.DecodeString(pubKeyHex)
+	if err != nil {
+		return "", err
+	}
+	pk, err := btcec.ParsePubKey(bs, btcec.S256())
+	if err != nil {
+		return "", err
+	}
+	cpk := pk.SerializeCompressed()
+	accAddress, err := sdk.AccAddressFromHex(hex.EncodeToString(cpk))
+	if err != nil {
+		return "", err
+	}
+	return accAddress.String(), nil
 }
