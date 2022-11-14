4fb65535b2a7871e2035b205eddc47251e407fbb
jowenshaw <jowenshaw@gmail.com>
2022-11-14 10:51:07 +0800

    update check kusama block confirmations

--- tokens/kusama/callapi.go
+++ tokens/kusama/callapi.go
@@ -5,6 +5,7 @@ import (
 	"github.com/anyswap/CrossChain-Bridge/common/hexutil"
 	"github.com/anyswap/CrossChain-Bridge/rpc/client"
 	"github.com/anyswap/CrossChain-Bridge/tokens"
+	"github.com/anyswap/CrossChain-Bridge/types"
 )
 
 var (
@@ -13,9 +14,28 @@ var (
 
 // ------------------------ kusama override apis -----------------------------
 
-// GetLatestBlockNumberOf call eth_blockNumber
-func (b *Bridge) GetLatestBlockNumberOf(url string) (latest uint64, err error) {
-	blockHash, err := b.KsmGetFinalizedHead(url)
+// KsmHeader struct
+type KsmHeader struct {
+	ParentHash *common.Hash `json:"parentHash"`
+	Number     *hexutil.Big `json:"number"`
+}
+
+// GetBlockConfirmations override this method
+func (b *Bridge) GetBlockConfirmations(receipt *types.RPCTxReceipt) (uint64, error) {
+	latest, err := b.GetFinalizedBlockNumber()
+	if err != nil {
+		return 0, err
+	}
+	blockNumber := receipt.BlockNumber.ToInt().Uint64()
+	if latest > blockNumber {
+		return latest - blockNumber, nil
+	}
+	return 0, nil
+}
+
+// GetFinalizedBlockNumber call chain_getFinalizedHead and chain_getHeader
+func (b *Bridge) GetFinalizedBlockNumber() (uint64, error) {
+	blockHash, err := b.KsmGetFinalizedHead()
 	if err != nil {
 		return 0, err
 	}
@@ -29,36 +49,25 @@ func (b *Bridge) GetLatestBlockNumberOf(url string) (latest uint64, err error) {
 // ------------------------ kusama specific apis -----------------------------
 
 // KsmGetFinalizedHead call chain_getFinalizedHead
-func (b *Bridge) KsmGetFinalizedHead(url string) (result common.Hash, err error) {
-	err = client.RPCPost(&result, url, "chain_getFinalizedHead")
-	if err == nil {
-		return result, nil
+func (b *Bridge) KsmGetFinalizedHead() (result *common.Hash, err error) {
+	urls := append(b.GatewayConfig.APIAddress, b.GatewayConfig.APIAddressExt...)
+	for _, url := range urls {
+		err = client.RPCPost(&result, url, "chain_getFinalizedHead")
+		if err == nil && result != nil {
+			return result, nil
+		}
 	}
-	return result, wrapRPCQueryError(err, "chain_getFinalizedHead")
-}
-
-// KsmHeader struct
-type KsmHeader struct {
-	ParentHash *common.Hash `json:"parentHash"`
-	Number     *hexutil.Big `json:"number"`
+	return nil, wrapRPCQueryError(err, "chain_getFinalizedHead")
 }
 
 // KsmGetHeader call chain_getHeader
 func (b *Bridge) KsmGetHeader(blockHash string) (result *KsmHeader, err error) {
-	gateway := b.GatewayConfig
-	result, err = b.ksmGetHeader(blockHash, gateway.APIAddress)
-	if err != nil && len(gateway.APIAddressExt) > 0 {
-		result, err = b.ksmGetHeader(blockHash, gateway.APIAddressExt)
-	}
-	return result, err
-}
-
-func (b *Bridge) ksmGetHeader(blockHash string, urls []string) (result *KsmHeader, err error) {
+	urls := append(b.GatewayConfig.APIAddress, b.GatewayConfig.APIAddressExt...)
 	for _, url := range urls {
 		err = client.RPCPost(&result, url, "chain_getHeader", blockHash)
 		if err == nil && result != nil {
 			return result, nil
 		}
 	}
-	return nil, wrapRPCQueryError(err, "chain_getHeader")
+	return nil, wrapRPCQueryError(err, "chain_getHeader", blockHash)
 }
